0d7b91496ddf7ccb8390b7c48aa3e693c7270645
BulgakovDmitry <dima.olympiad11@yandex.ru>
2025-12-30 03:08:06 +0300

    [test/end_to_end/] I am adding tests in which some data is submitted to the program input and the result is checked

--- frontend/tests/end_to_end/test_assign_in_expr/test_assign_in_expr.sh
+++ frontend/tests/end_to_end/test_assign_in_expr/test_assign_in_expr.sh
@@ -6,4 +6,11 @@ TEST_PATH="../frontend/tests/end_to_end/test_assign_in_expr/assign_in_expr.txt"
 out=$("$PROGRAM" "$TEST_PATH")
 
 norm=$(printf "%s" "$out" | tr -s '[:space:]' ' ' | sed 's/^ //; s/ $//')
-[ "$norm" = "1 2 4" ]
+
+if [ "$norm" = "1 2 4" ]; then
+  echo "test_assign_in_expr success"
+  exit 0
+else
+  echo "test_assign_in_expr fail"
+  exit 1
+fi
--- frontend/tests/end_to_end/test_bitwise_op/test_bitwise_op.sh
+++ frontend/tests/end_to_end/test_bitwise_op/test_bitwise_op.sh
@@ -6,4 +6,11 @@ TEST_PATH="../frontend/tests/end_to_end/test_bitwise_op/bitwise_op.txt"
 out=$("$PROGRAM" "$TEST_PATH")
 
 norm=$(printf "%s" "$out" | tr -s '[:space:]' ' ' | sed 's/^ //; s/ $//')
-[ "$norm" = "1 1 1 1 1 1 1 1 1" ]
+
+if [ "$norm" = "1 1 1 1 1 1 1 1 1" ]; then
+  echo "test_bitwise_op success"
+  exit 0
+else
+  echo "test_bitwise_op fail"
+  exit 1
+fi
