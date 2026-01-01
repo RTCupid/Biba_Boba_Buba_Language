b7dfd077259bfe2738ae5a17d53ae465263dffe4
GitHub Action <action@github.com>
2026-01-01 13:40:09 +0000

    Auto-format code

--- frontend/include/error_collector.hpp
+++ frontend/include/error_collector.hpp
@@ -14,23 +14,23 @@ class Error_collector {
         yy::location loc_;
         std::string msg_;
 
-        Error_info (yy::location &loc, std::string &msg) : loc_(loc), msg_(msg) {}
+        Error_info(yy::location &loc, std::string &msg)
+            : loc_(loc), msg_(msg) {}
 
         void print(std::ostream &os) const {
-            os << "Syntax error at line " << loc_.begin.line
-              << ", column " << loc_.begin.column << ": " << msg_ << '\n';
+            os << "Syntax error at line " << loc_.begin.line << ", column "
+               << loc_.begin.column << ": " << msg_ << '\n';
         }
     };
 
     std::vector<Error_info> errors_;
-public:
+
+  public:
     void add_error(yy::location &loc, std::string &msg) {
         errors_.push_back(Error_info{loc, msg});
     }
 
-    bool has_errors() const {
-        return !errors_.empty();
-    }
+    bool has_errors() const { return !errors_.empty(); }
 
     void print_errors(std::ostream &os) const {
         if (!errors_.empty())
