6f68a0c8e7926a11180ab04a08bb1e684e77ac36
Alexey <lavrik-aleksey-1@yandex.ru>
2026-02-22 12:41:45 +0300

    remove comments

--- frontend/include/node_pool.hpp
+++ frontend/include/node_pool.hpp
@@ -1,30 +1,3 @@
-// #ifndef FRONTEND_INCLUDE_AST_FACTORY_HPP
-// #define FRONTEND_INCLUDE_AST_FACTORY_HPP
-
-// #include "config.hpp"
-// #include "iterative_ast_deleter.hpp"
-// #include "node.hpp"
-// #include <memory>
-// #include <vector>
-
-// namespace language {
-
-// class AST_Factory final {
-//   public:
-//     static program_ptr makeProgram(StmtList stmts) {
-//         return program_ptr(new Program(std::move(stmts)));
-//     }
-
-//     template <typename T, typename... Args>
-//     static std::unique_ptr<T> make(Args &&...args) {
-//         return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-//     }
-// };
-
-// } // namespace language
-
-// #endif // FRONTEND_INCLUDE_AST_FACTORY_HPP
-
 #ifndef FRONTEND_INCLUDE_AST_FACTORY_HPP
 #define FRONTEND_INCLUDE_AST_FACTORY_HPP
 
